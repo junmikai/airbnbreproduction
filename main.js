096cc176b0193cb343bdf9a4540c2f70d2e40d5d
junmikai <sizimi_ga_toruru@yahoo.co.jp>
2020-05-19 21:53:12 +0900

    ヘッダー固定

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-// メニューヘッダーを最下層までスクロールした時非表示にする
+$( window ).on( 'scroll', function() {
+  if ( 50 < jQuery( this ).scrollTop() ) { // 1000px以上スクロールしたら
+    $( '.topheader' ).addClass( 'm_fixed' );
+  } else {
+    $( '.topheader' ).removeClass( 'm_fixed' );
+  }
+});
 
+// メニューヘッダーを最下層までスクロールした時非表示にする
 $(window).on('scroll', function () {
   var doch = $(document).innerHeight(); 
   var winh = $(window).innerHeight(); 
