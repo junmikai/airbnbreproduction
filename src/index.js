68278ae921f885f986cd0a89963821c699d9e4f1
junmikai <sizimi_ga_toruru@yahoo.co.jp>
2020-05-28 12:35:45 +0900

    オンライン体験webpack対応+フォルダ整理

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import "./style.scss";
-import "./responsive.scss";
-import "./tools.scss";
-import "./settings.scss";
+import "./sass/index/style.scss";
+import "./sass/index/responsive.scss";
+import "./sass/index/tools.scss";
+import "./sass/index/settings.scss";
+
+import "./sass/experiences/style.scss";
+import "./sass/experiences/responsive.scss";
+import "./sass/experiences/mixin.scss";
