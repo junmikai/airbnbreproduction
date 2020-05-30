42ac6d3179346cf548ae86665de8446a1d76d3b4
junmikai <sizimi_ga_toruru@yahoo.co.jp>
2020-05-30 14:56:06 +0900

    カラー変数定義

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import "./sass/index/style.scss";
 import "./sass/index/responsive.scss";
 import "./sass/index/mixin.scss";
-import "./sass/index/settings.scss";
-import "./sass/index/color.scss";
 
 import "./sass/experiences/style.scss";
 import "./sass/experiences/responsive.scss";
 import "./sass/experiences/mixin.scss";
 
 import "./sass/reset.scss";
+import "./sass/color.scss";
