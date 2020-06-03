406df8afa6930a20e38fbd5ffc8d8080142bde86
junmikai <sizimi_ga_toruru@yahoo.co.jp>
2020-06-03 17:00:31 +0900

    オンライン体験ページのscssファイル分け

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./sass/index/objects.scss";
 import "./sass/index/responsive.scss";
 
-import "./sass/experiences/style.scss";
+import "./sass/experiences/objects.scss";
 import "./sass/experiences/responsive.scss";
 
 import "./sass/host/style.scss";
