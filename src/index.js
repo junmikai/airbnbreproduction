b969401062cc94b76bf334d5e5df6399fb64b6e9
junmikai <sizimi_ga_toruru@yahoo.co.jp>
2020-06-03 18:27:30 +0900

    【ホスト】夢を共有する仲間
    sassファイル分けも同時に実施済みです

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./sass/index/responsive.scss";
 import "./sass/experiences/objects.scss";
 import "./sass/experiences/responsive.scss";
 
-import "./sass/host/style.scss";
+import "./sass/host/objects.scss";
 
 import "./sass/reset.scss";
 import "./sass/color.scss";
