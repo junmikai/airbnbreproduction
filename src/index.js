cf7fd04e88c068aebe242fdfb5d8c10c43655a8e
junmikai <sizimi_ga_toruru@yahoo.co.jp>
2020-06-04 12:16:51 +0900

    【ホスト】レスポンシブ対応/ヘッダー&トップ背景画像

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import "./sass/experiences/objects.scss";
 import "./sass/experiences/responsive.scss";
 
 import "./sass/host/objects.scss";
+import "./sass/host/responsive.scss";
 
 import "./sass/reset.scss";
 import "./sass/color.scss";
